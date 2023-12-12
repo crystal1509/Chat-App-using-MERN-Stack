77e60c053f17fbafefacc49e56632010ce644701
crystal1509 <gsbhatti2001@gmail.com>
2023-12-13 02:42:43 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 // const cors = require("cors");
 
 dotenv.config();
@@ -38,6 +39,25 @@ app.use("/api/message", messageRoutes);
 //   const singleChat=chats.find((c)=> c._id===req.params.id);
 //   res.send(singleChat);
 // });
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
 app.use(notFound);
 app.use(errorHandler);
 
@@ -87,4 +107,3 @@ io.on("connection", (socket) => {
     socket.leave(userData._id);
   });
 });
-
