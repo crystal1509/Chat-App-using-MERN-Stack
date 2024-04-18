1404bf0dd35b39b02c2b2ec5e7f9d1cf9e2de4bb
crystal1509 <gsbhatti2001@gmail.com>
2024-04-18 19:51:24 +0530

    update

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -37,7 +37,7 @@ const Homepage=()=> {
         borderWidth="1px"
       >
         <Text fontSize="4xl" fontFamily="Work sans" textAlign={"center"}>
-          Talk-A-Tive
+          EchoChat
         </Text>
       </Box>
       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -368,7 +368,7 @@ function SideDrawer() {
           </Button>
         </Tooltip>
         <Text fontSize="2xl" fontFamily="Work sans">
-          Talk-A-Tive
+          EchoChat
         </Text>
         <div>
           <Menu>
